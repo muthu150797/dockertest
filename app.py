f6044a83a41a91df9a2e21052963e0f9213bbe73
muthu150797 <muthumani150797@gmail.com>
2025-09-11 15:59:16 +0530

    xcvxcv

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from flask import Flask
-import face_recognition
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello from Flask inside Docker on Render!"
-
-@app.route('/GetfaceRecogV')
-def GetfaceRecogV():
-    return "face recognition version is "+face_recognition.__version__
 
 @app.route('/getme')
 def sayhi():
